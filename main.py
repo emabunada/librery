18c42a8be38dea90b3ca7644f79c974e54993696
emabunada <e.mabunada@gmail.com>
2022-07-17 21:12:24 +0300

    17/07/2022-1.0

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from client import Client
+from book import Book
 from borrowing_order import BorrowingOrder
+from menus import Menus
 
-c = Client('ahmed', 18, 'asdsadas', '6545646464')
-b = BorrowingOrder('1', '2', 20, )
-print(b.get_end_date())
+clients = [Client('mohammed', 22, '805698744', '0598756322'), Client('Khaled', 18, '405688799', '0568744158'),
+           Client('Alaa', 30, '700598776', '0592478965'), Client('Khalil', 25, '778410259', '0598753214'), ]
+books = [Book('Java how to program', 'Paul Deitel', ), Book('Queen of Fire', 'Devika Rangachari', ),
+         Book('The India Story', '	Bimal Jalal', ), Book('Hear Yourself', 'Prem Rawat', ),
+         Book('A Place Called Home', 'Preeti Shenoy', )]
+Menus().main_menu()
